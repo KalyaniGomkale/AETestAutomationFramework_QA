fd3e5e371b0abd3d0dc6a4463e926d5fb8904506
Rohil7299 <rohil.kumbhar06@gmail.com>
2023-03-02 12:00:59 +0530

    Changed in Delete agent[TA] testscript no manual intervation now RK

--- src/main/java/com/ae/qa/pagesTenantAdmin/AgentListPageTA.java
+++ src/main/java/com/ae/qa/pagesTenantAdmin/AgentListPageTA.java
@@ -377,52 +377,65 @@ public class AgentListPageTA extends TestBase{
 		JavascriptExecutor js=(JavascriptExecutor)driver;
 		js.executeScript("arguments[0].click();", AgentsTab);
 		Reporter.log("User navigated to Agents Tab",true);
-		Thread.sleep(2000);
-		for(int i=0;i<1;i++) {
-			refreshTable.click();	
-			String AgentStatus=status.getText();
-			Reporter.log("Current status of Agent is: "+AgentStatus);
-			Thread.sleep(3000);
-			if(AgentStatus.equals("Running")) {
-				Assert.assertEquals(AgentStatus,"Running","Agent is not in running mode");
-				break;
-			}
+		Thread.sleep(3000);
+		for(int i=0;i<4;i++) {
+			refreshTable.click();
+		}
+		Thread.sleep(3000);
+		String AgentStatus=status.getText();
+		Reporter.log("Current status of Agent is: "+AgentStatus);
+		Thread.sleep(3000);
+		if(AgentStatus.equals("Running")) {
+			Assert.assertEquals(AgentStatus,"Running","Agent is not in running mode");
 		}
 		Reporter.log("Agent is in running mode",true);
 		Thread.sleep(2000);
-		//WebElement stop_btn = driver.findElement(By.xpath("//table/tr/td[contains(text(),'"+agentName+"')]/../button[@title='Stop Agent']"));
-		WebElement stop_btn= driver.findElement(By.xpath("//button[@title='Stop Agent']"));
+		WebElement stop_btn= driver.findElement(By.xpath("//table/tr/td/span[text()='"+agentName+"']/../../td/button[@title='Stop Agent']"));
 		stop_btn.click();
 		Reporter.log("Stop Button is Clicked",true);
 		Thread.sleep(2000);
 		confirmStopBtn.click();
 		Reporter.log("Confirm Stop Button is Clicked",true);
+		Thread.sleep(2000);
+		String actual_Message=alertMessage.getText();
+		Reporter.log("Actual_Message:-"+actual_Message,true);
+		String expected_Message="Stopping agent ["+agentName+"] is in progress";
+		Reporter.log("Expected Message:- "+expected_Message,true);
+		Assert.assertEquals(actual_Message, expected_Message,"Agent not deleted successfully");
+		Reporter.log("Agent is stopped successfully",true);
+		informationpageta.validateSignOut();
+		loginpage.login(prop.getProperty("username_TA1"), prop.getProperty("password_TA1"));
+		Reporter.log("User LogIn Succesfully",true);
+		//wait.until(ExpectedConditions.visibilityOf(AgentsTab));
+		Thread.sleep(5000);
+		js.executeScript("arguments[0].click();", AgentsTab);
+		Reporter.log("User navigated to Agents Tab",true);
 		Thread.sleep(10000);
-		for(int i=0;i<2;i++) {
-			refreshTable.click();	
-			String AgentStatus=status.getText();
-			Reporter.log("Current status of Agent is: "+AgentStatus);
-			Thread.sleep(3000);
-			if(AgentStatus.equals("Stopped")) {
-				Assert.assertEquals(AgentStatus,"Stopped","Agent is not in Stopped mode");
-				break;
-			}
+		for(int i=0;i<4;i++) {
+			refreshTable.click();
+		}
+		Thread.sleep(3000);
+		String AgentStatus1=status.getText();
+		Reporter.log("Current status of Agent is: "+AgentStatus);
+		Thread.sleep(3000);
+		if(AgentStatus1.equals("Stopped")) {
+			Assert.assertEquals(AgentStatus1,"Stopped","Agent is not in Stopped mode");
 		}
 		Reporter.log("Agent is in Stopped mode",true);
 		Thread.sleep(2000);
-		//WebElement delete_btn = driver.findElement(By.xpath("//table/tr/td[contains(text(),'"+agentName+"')]/../button[@title='Delete Agent']"));
-		WebElement delete_btn= driver.findElement(By.xpath("//button[@title='Delete Agent']"));
+		
+		WebElement delete_btn= driver.findElement(By.xpath("//table/tr/td/span[text()='"+agentName+"']/../../td/button[@title='Delete Agent']"));
 		delete_btn.click();
 		Reporter.log("Delete Button is Clicked",true);
 		Thread.sleep(2000);
 		confirmDeleteBtn.click();
 		Reporter.log("Confirm Delete Button is Clicked",true);
 		Thread.sleep(4000);
-		String actual_Message=alertMessage.getText();
-		System.out.println("Actual_Message:-"+actual_Message);
-		String expected_Message=Messages.deleteAgent;
-		System.out.println("Expected Message:- "+expected_Message);
-		Assert.assertEquals(actual_Message, expected_Message,"Agent not deleted successfully");
+		String actual_Message1=alertMessage.getText();
+		System.out.println("Actual_Message:-"+actual_Message1);
+		String expected_Message1="Agent ["+agentName+"] deleted successfully";
+		System.out.println("Expected Message:- "+expected_Message1);
+		Assert.assertEquals(actual_Message1, expected_Message1,"Agent not deleted successfully");
 		Reporter.log("Agent is deleted successfully",true);
 		informationpageta.validateSignOut();
 	}
@@ -434,56 +447,69 @@ public class AgentListPageTA extends TestBase{
 		JavascriptExecutor js=(JavascriptExecutor)driver;
 		js.executeScript("arguments[0].click();", AgentsTab);
 		Reporter.log("User navigated to Agents Tab",true);
-		Thread.sleep(4000);
 		js.executeScript("arguments[0].click();", AssistedAgentTab);
 		Reporter.log("User navigated to Assisted Agents Tab",true);
-		Thread.sleep(2000);
-		for(int i=0;i<1;i++) {
-			refreshTable.click();	
-			String AgentStatus=status.getText();
-			Reporter.log("Current status of Assisted Agent is: "+AgentStatus);
-			Thread.sleep(3000);
-			if(AgentStatus.equals("Running")) {
-				Assert.assertEquals(AgentStatus,"Running","Assisted Agent is not in running mode");
-				break;
-			}
+		Thread.sleep(3000);
+		for(int i=0;i<4;i++) {
+			refreshTable.click();
 		}
-		Reporter.log("Assisted Agent is in running mode",true);
+		Thread.sleep(3000);
+		String AgentStatus=status.getText();
+		Reporter.log("Current status of Agent is: "+AgentStatus);
+		Thread.sleep(3000);
+		if(AgentStatus.equals("Running")) {
+			Assert.assertEquals(AgentStatus,"Running","Agent is not in running mode");
+		}
+		Reporter.log("Agent is in running mode",true);
 		Thread.sleep(2000);
-		//WebElement stop_btn = driver.findElement(By.xpath("//table/tr/td[contains(text(),'"+agentName+"')]/../button[@title='Stop Agent']"));
-		WebElement stop_btn= driver.findElement(By.xpath("//button[@title='Stop Agent']"));
+		WebElement stop_btn= driver.findElement(By.xpath("//table/tr/td/span[text()='"+agentName+"']/../../td/button[@title='Stop Agent']"));
 		stop_btn.click();
 		Reporter.log("Stop Button is Clicked",true);
 		Thread.sleep(2000);
 		confirmStopBtn.click();
 		Reporter.log("Confirm Stop Button is Clicked",true);
+		Thread.sleep(2000);
+		String actual_Message=alertMessage.getText();
+		Reporter.log("Actual_Message:-"+actual_Message,true);
+		String expected_Message="Stopping agent ["+agentName+"] is in progress";
+		Reporter.log("Expected Message:- "+expected_Message,true);
+		Assert.assertEquals(actual_Message, expected_Message,"Agent not deleted successfully");
+		Reporter.log("Agent is stopped successfully",true);
+		informationpageta.validateSignOut();
+		loginpage.login(prop.getProperty("username_TA1"), prop.getProperty("password_TA1"));
+		Reporter.log("User LogIn Succesfully",true);
+		//wait.until(ExpectedConditions.visibilityOf(AgentsTab));
+		Thread.sleep(5000);
+		js.executeScript("arguments[0].click();", AgentsTab);
+		Reporter.log("User navigated to Agents Tab",true);
+		js.executeScript("arguments[0].click();", AssistedAgentTab);
+		Reporter.log("User navigated to Assisted Tab",true);
 		Thread.sleep(10000);
-		for(int i=0;i<2;i++) {
-			refreshTable.click();	
-			String AgentStatus=status.getText();
-			Reporter.log("Current status of Assisted Agent is: "+AgentStatus);
-			Thread.sleep(5000);
-			if(AgentStatus.equals("Stopped")) {
-				Assert.assertEquals(AgentStatus,"Stopped","Assisted Agent is not in Stopped mode");
-				break;
-			}
+		for(int i=0;i<4;i++) {
+			refreshTable.click();
+		}
+		Thread.sleep(3000);
+		String AgentStatus1=status.getText();
+		Reporter.log("Current status of Agent is: "+AgentStatus);
+		Thread.sleep(3000);
+		if(AgentStatus1.equals("Stopped")) {
+			Assert.assertEquals(AgentStatus1,"Stopped","Agent is not in Stopped mode");
 		}
 		Reporter.log("Agent is in Stopped mode",true);
 		Thread.sleep(2000);
-		//WebElement delete_btn = driver.findElement(By.xpath("//table/tr/td[contains(text(),'"+agentName+"')]/../button[@title='Delete Agent']"));
-		WebElement delete_btn= driver.findElement(By.xpath("//button[@title='Delete Agent']"));
+		WebElement delete_btn= driver.findElement(By.xpath("//table/tr/td/span[text()='\"+agentName+\"']/../../td/button[@title='Delete Agent']"));
 		delete_btn.click();
 		Reporter.log("Delete Button is Clicked",true);
 		Thread.sleep(2000);
 		confirmDeleteBtn.click();
 		Reporter.log("Confirm Delete Button is Clicked",true);
 		Thread.sleep(4000);
-		String actual_Message=alertMessage.getText();
-		System.out.println("Actual_Message:-"+actual_Message);
-		String expected_Message=Messages.deleteAgent;
-		System.out.println("Expected Message:- "+expected_Message);
-		Assert.assertEquals(actual_Message, expected_Message,"Assisted Agent not deleted successfully");
-		Reporter.log("Assisted Agent is deleted successfully",true);
+		String actual_Message1=alertMessage.getText();
+		System.out.println("Actual_Message:-"+actual_Message1);
+		String expected_Message1="Agent ["+agentName+"] deleted successfully";
+		System.out.println("Expected Message:- "+expected_Message1);
+		Assert.assertEquals(actual_Message1, expected_Message1,"Agent not deleted successfully");
+		Reporter.log("Agent is deleted successfully",true);
 		informationpageta.validateSignOut();
 	}
 	public void unassignAssistedAgentToUser(String Username,String password,String tabName) throws Exception {
